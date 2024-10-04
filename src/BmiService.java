8ce05889eb8674a9afc77e9f2617ac69644cbb22
Daria <Dasha-esha@mail.ru>
2024-10-04 15:06:29 +0500

    update

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public int calculate(int weightKg, double heightM) {
+        double result = weightKg / heightM / heightM;
+        return (int) result;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        BmiService service = new BmiService();
+        int weghtKg = 98;
+        double heightM = 1.87;
+        int BMI = service.calculate(weghtKg, heightM);
+        System.out.println(BMI);
     }
 }
