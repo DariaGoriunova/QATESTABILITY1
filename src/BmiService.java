d1e88be5742e6e8c909c71ce854170d54ed6ba24
Daria <Dasha-esha@mail.ru>
2024-10-04 15:12:21 +0500

    update

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weightKg, double heightM) {
-        double result = weightKg / heightM / heightM;
+        double result = weightKg / Math.pow(heightM, 2);
         return (int) result;
     }
 
